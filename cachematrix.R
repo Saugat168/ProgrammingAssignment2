a983e21af0772ab20d05bee7b2439b253c066f6b
Saugat <saugat168@gmail.com>
2017-08-09 23:06:58 +0200

    Finished the code for MakeCacheMatrix and CacheSolvematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The set function initializes the matrix and its corresponding inverse matrix
+## The inverse matrix is initialized with NA(s)
+## The set and get inverse functions saves and fetches the inverse of the matrix respectively
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- matrix(NA,nrow(x),ncol(x))
+  set <- function(y) {
+    x <<- y
+    inv <- matrix(NA,nrow(x),ncol(x))
+  }
+  get <- function() x
+  setInverse <- function(Inverse) inv <<- Inverse
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve methods checks if the inverse is already cached
+## It does that by checking for any NAs in the matrix as that is how it was initialized when the inverse wasn't calculated
+## When it sees that it is not cached, it calculates it and saves it for later use
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(any(!is.na(inv))) {
+    message("getting cached inversed matrix")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
 }
